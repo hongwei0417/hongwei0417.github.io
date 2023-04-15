89d6a5b6d3b4f6763e89f6fbb2049b1690eaac29
Kevin Hu <iamhongwei0417@gmail.com>
2023-04-15 21:48:38 +0800

    update github

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,15 +11,16 @@ const config = {
 	favicon: "img/favicon.ico",
 
 	// Set the production url of your site here
-	url: "https://your-docusaurus-test-site.com",
+	url: "https://github.com/hongwei0417",
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
 	baseUrl: "/",
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
-	organizationName: "facebook", // Usually your GitHub org/user name.
-	projectName: "docusaurus", // Usually your repo name.
+	organizationName: "hongwei0417", // Usually your GitHub org/user name.
+	projectName: "hongwei0417.github.io", // Usually your repo name.
+	trailingSlash: false,
 
 	onBrokenLinks: "throw",
 	onBrokenMarkdownLinks: "warn",
@@ -96,7 +97,7 @@ const config = {
 						items: [
 							{
 								label: "Tutorial",
-								to: "/docs/intro",
+								to: "/intro",
 							},
 						],
 					},
